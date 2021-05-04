13c4999786cafef52aab223b4645c338408f878e
Júlio Faria <julio-pinheiroo@hotmail.com>
2021-05-04 12:27:14 -0300

    Testes do arquivo 1_unit-tests.js feitos

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -52,9 +52,57 @@ suite('Unit Tests', function () {
   })
 
   test('convertHandler should correctly return an error for an invalid input unit.', function () {
-    assert.isFalse(Boolean('5/2kGg'.match(validUnitRegex)))
-    assert.isFalse(Boolean('4.2GalL'.match(validUnitRegex)))
+    assert.isFalse(Boolean('5/2kGgmI'.match(validUnitRegex)))
+    assert.isFalse(Boolean('4.2gGal'.match(validUnitRegex)))
     assert.isFalse(Boolean('37m'.match(validUnitRegex)))
+  })
+
+  test('convertHandler should return the correct return unit for each valid input unit.', function () {
+    assert.equal(convertHandler.getReturnUnit('kg'), 'lbs')
+    assert.equal(convertHandler.getReturnUnit('gal'), 'L')
+    assert.equal(convertHandler.getReturnUnit('mi'), 'km')
+  })
+
+  test('convertHandler should correctly return the spelled-out string unit for each valid input unit.', function () {
+    assert.equal(convertHandler.spellOutUnit('kg'), 'kilograms')
+    assert.equal(convertHandler.spellOutUnit('gal'), 'gallons')
+    assert.equal(convertHandler.spellOutUnit('mi'), 'miles')
+  })
+
+  test('convertHandler should correctly convert gal to L.', function () {
+    assert.equal(convertHandler.convert(10,'gal'), 37.85410)
+    assert.equal(convertHandler.convert(21.5,'gal'), 81.38632)
+    assert.equal(convertHandler.convert(44.98,'gal'), 170.26774)
+  })
+
+  test('convertHandler should correctly convert L to gal.', function () {
+    assert.equal(convertHandler.convert(4,'L'), 1.05669)
+    assert.equal(convertHandler.convert(33.1,'L'), 8.74410)
+    assert.equal(convertHandler.convert(12.65,'L'), 3.34178)
+  })
+
+  test('convertHandler should correctly convert mi to km.', function () {
+    assert.equal(convertHandler.convert(7,'mi'), 11.26538)
+    assert.equal(convertHandler.convert(20.36,'mi'), 32.76616)
+    assert.equal(convertHandler.convert(12.61,'mi'), 20.29378)
+  })
+
+  test('convertHandler should correctly convert km to mi.', function () {
+    assert.equal(convertHandler.convert(2,'km'), 1.24275)
+    assert.equal(convertHandler.convert(23.423,'km'), 14.55441)
+    assert.equal(convertHandler.convert(432.21,'km'), 268.56351)
+  })
+
+  test('convertHandler should correctly convert lbs to kg.', function () {
+    assert.equal(convertHandler.convert(4,'lbs'), 1.81437)
+    assert.equal(convertHandler.convert(41.008,'lbs'), 18.60090)
+    assert.equal(convertHandler.convert(12.56,'lbs'), 5.69712)
+  })
+
+  test('convertHandler should correctly convert kg to lbs.', function () {
+    assert.equal(convertHandler.convert(8.1,'kg'), 17.85746)
+    assert.equal(convertHandler.convert(21.73,'kg'), 47.90649)
+    assert.equal(convertHandler.convert(31.44,'kg'), 69.31339)
   })
 
 });
